4393608a8663e3426c0804e636fbdc025dfd56b3
adham nabil <85515550+adhmnabil@users.noreply.github.com>
2022-06-11 20:48:00 -0400

    Add files via upload

---
+++ precache-manifest.be971c1d583e19af829a9d4cd9775580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cd2018aaac0db5e368b",
+    "url": "./static/css/main.fa62f85d.chunk.css"
+  },
+  {
+    "revision": "9cd2018aaac0db5e368b",
+    "url": "./static/js/main.9cd2018a.chunk.js"
+  },
+  {
+    "revision": "b77af2751d63109036cf",
+    "url": "./static/css/1.84d7f6ac.chunk.css"
+  },
+  {
+    "revision": "b77af2751d63109036cf",
+    "url": "./static/js/1.b77af275.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "695c234114d9c94a69e9772aa51fea7e",
+    "url": "./index.html"
+  }
+];
